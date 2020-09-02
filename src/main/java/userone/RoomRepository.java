a24f14dded0e09f4bc7e7bf0dcc5464831748275
s0wnd <aimmvp@gmail.com>
2020-09-02 15:17:33 +0900

    2nd commit

---
+++ src/main/java/userone/RoomRepository.java
@@ -0,0 +1,8 @@
+package userone;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface RoomRepository extends PagingAndSortingRepository<Room, Long>{
+
+
+}
---
+++ src/main/java/userone/external/BookingService.java
@@ -0,0 +1,17 @@
+
+package userone.external;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Date;
+
+@FeignClient(name="booking", url="http://booking:8080")
+public interface BookingService {
+
+    @RequestMapping(method= RequestMethod.PATCH, path="/bookings")
+    public void bookingChange(@RequestBody Booking booking);
+
+}
